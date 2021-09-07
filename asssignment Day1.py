bd7e313283d68a824e253801770ba5bf3973a3d9
Sri-Sandeep <90254843+Sri-Sandeep@users.noreply.github.com>
2021-09-07 16:52:19 +0530

    Day 1 Assignment Submitted

---
+++ asssignment Day1.py
@@ -0,0 +1,19 @@
+a=29
+b=7
+print("addition of the a and b are",a+b)
+print("subtaction of the a and b is",a-b)
+print("the value of division",a/b)
+print("the value of remainder",a%b)
+print("the value with out  float",a//b)
+print("the value of product",a*b)
+print("power of given value",a**b)
+print((a*b)+(a/b))
+
+# To obtain the simple interest
+principle=int(input("enter the value\n"))
+rate=int(input("enter the value\n"))
+time=int(input("enter the value\n"))
+simple_interset=(principle*rate*time)/100
+print("simple_interset is",simple_interset)
+
+
